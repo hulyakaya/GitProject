5deb545c59fcd5bc7792b222d543d371f15c1d8a
Hulya Kaya <ysfky24@gmail.com>
2022-01-27 12:58:16 -0500

    I updated first class.

--- gitproject/src/day01_git_project/GitExamples.java
+++ gitproject/src/day01_git_project/GitExamples.java
@@ -4,5 +4,6 @@ public class GitExamples {
 
     public static void main(String[] args) {
         System.out.println("learning git commits");
+        System.out.println("More code is coming");
     }
 }
